2d41c33f251eb597176b4ea26d13342d1365bf1a
Ayush Modi <Ayush472>
2026-02-18 12:41:25 +0530

    fix(faculty): ensure timetable is always a Map

--- app/actions/faculty.ts
+++ app/actions/faculty.ts
@@ -44,7 +44,7 @@ export async function createFacultyGroup(data: CreateFacultyGroupInput) {
             ...validatedData,
             timetable: validatedData.timetable
                 ? new Map(Object.entries(validatedData.timetable))
-                : undefined
+                : new Map()
         };
 
         const newGroup = await FacultyGroup.create(facultyData);
