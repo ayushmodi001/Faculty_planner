f1bfe02906941e8f408f222b2d730c95b3c352da
Ayush Modi <2303031057006@paruluniversity.ac.in>
2026-03-31 14:40:36 +0530

    fix nextHandler by setting output to standalone

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   serverExternalPackages: ['mongoose', 'bcryptjs'],
-  // AI generation can take a long time, extend fetch timeout if we use fetch() internally
-  // But this applies more to static generation.
 };
 
 export default nextConfig;
